9d9711324564a793725ce042a60e7b17e9e58f0c
adithiachristian7 <adithiachristian@gmail.com>
2024-10-29 07:41:20 +0800

    update

--- Exercise_8/index.js
+++ Exercise_8/index.js
@@ -1,4 +1,9 @@
-import { yearUntilRetirement, addNumber, calculateArea, makeAjaxRequest } from './utils.js';
+import {
+  yearUntilRetirement,
+  addNumber,
+  calculateArea,
+  makeAjaxRequest,
+} from "./utils.js";
 
 // Nomor 1
 yearUntilRetirement({ year: 1987, firstName: "John" });
@@ -15,5 +20,4 @@ const area7 = calculateArea({ radius });
 console.log(`area with 21 radius: ${area21}, and area with 7 radius: ${area7}`);
 
 // Nomor 4
-makeAjaxRequest('www.google.com');
-
+makeAjaxRequest("www.google.com");
